d3e5560c40a6114c55f0a8e3b352e103cb9780a2
Ashu95patil <patilashwini566@gmail.com>
2022-11-30 22:29:01 +0530

    second commit

--- two.java
+++ two.java
@@ -1,11 +1,15 @@
 package Addition;
 // very nice program.
+
+
+// first java program
 public class two {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
 		int c=a+b;
 		System.out.println(c);
+		
 		
 	}
 
